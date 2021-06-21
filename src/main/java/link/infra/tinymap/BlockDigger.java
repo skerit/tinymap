db74c69a9cb90c72e89f8bd0a2d07b498e6ad861
myste-rae <85805316+myste-rae@users.noreply.github.com>
2021-06-22 00:38:30 +0100

    Revert "Use custom world height"
    This reverts commit cbdffdb7c852166d8ec6bfe06bfc74059a51f393.

--- src/main/java/link/infra/tinymap/BlockDigger.java
+++ src/main/java/link/infra/tinymap/BlockDigger.java
@@ -150,9 +150,8 @@ class BlockDigger {
 						}
 					}
 				}
-				System.out.println(BlockDigger.this.world.getHeight());
-				System.out.println(BlockDigger.this.world.getBottomY());
-				Chunk unloadedChunkView = new UnloadedChunkView(sections,BlockDigger.this.world.getHeight(),BlockDigger.this.world.getBottomY());
+
+				Chunk unloadedChunkView = new UnloadedChunkView(sections);
 
 				NbtCompound heightmaps = level.getCompound("Heightmaps");
 				String heightmapName = Heightmap.Type.WORLD_SURFACE.getName();
